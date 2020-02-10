7b489f652e8fcbdfb62d4050503ee298b8106430
AsunaxRan <chimsedinangidol@gmail.com>
2020-02-10 14:51:34 +0700

    add pages

--- js/main.js
+++ js/main.js
@@ -22,3 +22,23 @@ $('.dropdown__toggle').on('click', function() {
 $('.sub-menu__toggle').on('click', function() {
   $(this).next().slideToggle();
 });
+
+// Functions
+function search(input) {
+  // Declare variables
+  var filter, ul, li, a, i, txtValue;
+  filter = input.toUpperCase();
+  ul = document.getElementsByClassName("list")[0];
+  li = ul.getElementsByTagName('li');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
